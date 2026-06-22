25f6d08b5195346c0a3abe465d3331c6fc1c0b06
ykfy1 <yktcyfy@foxmail.com>
2026-06-22 17:17:40 +0800

    Update proxy.ts

--- functions/proxy.ts
+++ functions/proxy.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "https://music-api.gdstudio.xyz/api.php";
+const DEFAULT_API_BASE_URL = "https://api.zjykfy.cc.cd";
 const KUWO_HOST_PATTERN = /(^|\.)kuwo\.cn$/i;
 const SAFE_RESPONSE_HEADERS = ["content-type", "cache-control", "accept-ranges", "content-length", "content-range", "etag", "last-modified", "expires"];
 
